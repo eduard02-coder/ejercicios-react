8e6c398fd084aa79a104df9ed3e9ed27b65c63b3
Eduard Oganesian <eo082002@gmail.com>
2026-09-23 17:01:21 -0300

    Change from BrowserRouter to RouterProvider Delete Landing.jsx and use App.jsx for landing page display

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { twMerge } from 'tailwind-merge';
-import { Routes, Route } from 'react-router-dom';
-import Landing from './components/pages/Landing';
-import Ejercicio1 from './components/pages/Ejercicio1/Ejercicio1';
-import Ejercicio2 from './components/pages/Ejercicio2/Ejercicio2';
-import Ejercicio3 from './components/pages/Ejercicio3/Ejercicio3';
-import Ejercicio4 from './components/pages/Ejercicio4/Ejercicio4';
-import Ejercicio5 from './components/pages/Ejercicio5/Ejercicio5';
-import Ejercicio6 from './components/pages/Ejercicio6/Ejercicio6';
-import Ejercicio7 from './components/pages/Ejercicio7/Ejercicio7';
-import Ejercicio8 from './components/pages/Ejercicio8/Ejercicio8';
-import Ejercicio9 from './components/pages/Ejercicio9/Ejercicio9';
+import Link from '../src/components/common/Link';
 
-function App() {
+const Landing = () => {
   return (
-    <div className={'prose pl-12 pt-12 pb-30'}>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/ejercicio1" element={<Ejercicio1 />} />
-        <Route path="/ejercicio2" element={<Ejercicio2 />} />
-        <Route path="/ejercicio3" element={<Ejercicio3 />} />
-        <Route path="/ejercicio4" element={<Ejercicio4 />} />
-        <Route path="/ejercicio5" element={<Ejercicio5 />} />
-        <Route path="/ejercicio6" element={<Ejercicio6 />} />
-        <Route path="/ejercicio7" element={<Ejercicio7 />} />
-        <Route path="/ejercicio8" element={<Ejercicio8 />} />
-        <Route path="/ejercicio9" element={<Ejercicio9 />} />
-      </Routes>
-    </div>
-  );
-}
+    <section className="landing ">
+      <title>Ejercicios - React</title>
+      <header>
+        <h1>Ejercicios - React</h1>
+        <h3>
+          Alumno: <span>Eduardo O.</span>
+        </h3>
+        <hr className="" />
+      </header>
+      <main>
+        <h2>Ejericios</h2>
 
-export default App;
+        <ol className="list-decimal list-inside">
+          <li>
+            <Link to="/ejercicio1">Cambiador de Color de Fondo</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio2">Contador de Clics</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio3">Lista Dinámica</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio4">Filtro de Búsqueda en Tiempo Real</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio5">Calculadora Sencilla</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio6">
+              Temporizador con Inicio, Pausa y Reinicio
+            </Link>
+          </li>
+          <li>
+            <Link to="/ejercicio7">Generador de Contraseñas Aleatorias</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio8">Contador de Palabras y Caracteres</Link>
+          </li>
+          <li>
+            <Link to="/ejercicio9">Lista de Tareas con LocalStorage</Link>
+          </li>
+        </ol>
+      </main>
+    </section>
+  );
+};
+
+export default Landing;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,38 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './index.css';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx';
+import Ejercicio1 from './components/pages/Ejercicio1/Ejercicio1';
+import Ejercicio2 from './components/pages/Ejercicio2/Ejercicio2';
+import Ejercicio3 from './components/pages/Ejercicio3/Ejercicio3';
+import Ejercicio4 from './components/pages/Ejercicio4/Ejercicio4';
+import Ejercicio5 from './components/pages/Ejercicio5/Ejercicio5';
+import Ejercicio6 from './components/pages/Ejercicio6/Ejercicio6';
+import Ejercicio7 from './components/pages/Ejercicio7/Ejercicio7';
+import Ejercicio8 from './components/pages/Ejercicio8/Ejercicio8';
+import Ejercicio9 from './components/pages/Ejercicio9/Ejercicio9';
+
+const router = createHashRouter([
+  { path: '/', element: <App /> },
+  { path: '/ejercicio1', element: <Ejercicio1 /> },
+  { path: '/ejercicio2', element: <Ejercicio2 /> },
+  { path: '/ejercicio3', element: <Ejercicio3 /> },
+  { path: '/ejercicio4', element: <Ejercicio4 /> },
+  { path: '/ejercicio5', element: <Ejercicio5 /> },
+  { path: '/ejercicio6', element: <Ejercicio6 /> },
+  { path: '/ejercicio7', element: <Ejercicio7 /> },
+  { path: '/ejercicio8', element: <Ejercicio8 /> },
+  { path: '/ejercicio9', element: <Ejercicio9 /> },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <div className={'prose pl-12 pt-12 pb-30'}>
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 );
